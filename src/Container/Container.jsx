1e622e8f6c38932dcc12be53c073550590d09be5
Mdforidulislam <mdforidulislam962@gmail.com>
2024-03-04 21:40:58 +0600

    aded new p tag

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -2,6 +2,7 @@ const Container = ({children}) => {
     return (
         <div className="max-w-screen-2xl mx-auto px-6 md:px-6 py-2">
             {children}
+            <p>hello world </p>
         </div>
     );
 };
