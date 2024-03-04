33be56b4028210c8dee3d17ab494068b3b386b54
Mdforidulislam <mdforidulislam962@gmail.com>
2024-03-04 22:51:17 +0600

    remove the p tag

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -2,7 +2,6 @@ const Container = ({children}) => {
     return (
         <div className="max-w-screen-2xl mx-auto px-6 md:px-6 py-2">
             {children}
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptatem delectus eum blanditiis quo sequi minus itaque animi assumenda debitis ut porro aut dolore, exercitationem deserunt sint hic numquam obcaecati asperiores qui minima, dolor molestiae impedit. Reiciendis sunt est beatae rerum unde mollitia eligendi tenetur suscipit, ullam temporibus consectetur libero id culpa maxime in quos cumque qui quod excepturi ratione similique? Distinctio quidem enim ullam, animi in reiciendis consequuntur dolor quod minus molestias! Reprehenderit nulla dolorem harum. Autem accusantium quidem, sint tempora fugit eaque possimus. Mollitia minus consequuntur eos veritatis! Rem nihil minus eos voluptas assumenda officiis consectetur asperiores corporis.</p>
         </div>
     );
 };
