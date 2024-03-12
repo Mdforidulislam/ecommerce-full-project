0eb33a7ad7aee04b7312a6afd58586dabb11f3e4
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-03-12 11:34:31 +0600

    registration and login page design done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import loginAnimation from './login.json'
 import Lottie from "lottie-react";
+import axios from "axios";
 
 const Login = () => {
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn, googleSignIn } = useContext(AuthContext);
+  const regiNavi = useNavigate()
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -35,6 +37,30 @@ const Login = () => {
           text: "An error occurred during sign in. Please try again.",
         });
       })
+  };
+
+  const handleGoogle = () => {
+    googleSignIn()
+      .then((result) => {
+        const userInfo = {
+          name: result.user.displayName,
+          email: result.user.email,
+          imageURL: result.user.photoURL
+        };
+        axios.post(`https://server-site-beta.vercel.app/register`, userInfo).then((res) => {
+          console.log(res);
+          if (res.statusText === 'OK') {
+            console.log('user added to the database'); 
+            Swal.fire({
+              icon: 'success',
+              title: 'Login Successful',
+              text: 'You have successfully logged in!'
+            });
+            regiNavi('/dashboard/user/profile');
+          }
+        });
+      })
+      .catch((error) => console.log(error));
   };
 
 
@@ -72,7 +98,7 @@ const Login = () => {
             <div className="mt-4 gap-10">
 
 
-              <button type="button" className="btn bg-[#0360D9] hover:bg-[#0360D9] text-white w-full">
+              <button onClick={handleGoogle} type="button" className="btn bg-[#0360D9] hover:bg-[#0360D9] text-white w-full">
                 <svg className="w-4 h-auto" width="46" height="47" viewBox="0 0 46 47" fill="none">
                   <path d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z" fill="#4285F4" />
                   <path d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z" fill="#34A853" />
@@ -85,7 +111,7 @@ const Login = () => {
             </div>
           </form>
 
-         
+
 
 
           <div className="mt-6 flex flex-col space-y-2">
